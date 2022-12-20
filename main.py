8fc190a6fb80c5bd3d718764ed54b721aa15ad65
Uiop 3385 <116633390+Uiop3385@users.noreply.github.com>
2022-12-20 17:14:33 +0000

    Update checker

--- main.py
+++ main.py
@@ -3,14 +3,27 @@ import codecs
 import os
 import cryptography
 from cryptography.fernet import Fernet
+import requests
+import time
+import webbrowser
 algorithm = "base64"
+currentVersion = "1.1"
 def menu():
   global algorithm
+  global newUpdate
+  global currentVersion
+  global version
   print("\n\n")
+  if newUpdate == True:
+    print("A new update can be downloaded now from the GitHub page!")
+    print("You're currently running", currentVersion, "and", version, "is available!", end = "\n\n")
   print("Welcome! Would you like to :", end = "\n\n")
   print("1 - Encrypt a file (selected algorithm : "+algorithm+")")
   print("2 - Decrypt a file (selected algorithm : "+algorithm+")")
-  print("3 - Select cipher algorithm", end = "\n\n")
+  print("3 - Select cipher algorithm")
+  if newUpdate == True:
+    print("4 - Download the updated version")
+  print("")
   choice = int(input("Enter your choice : "))
   if choice == 1:
     encrypt()
@@ -18,6 +31,9 @@ def menu():
     decrypt()
   elif choice == 3:
     select_algorithm()
+  elif choice == 4 and newUpdate == True:
+    link = "https://github.com/Uiop3385/PythonEncoderDecoder/releases/tag/" + version
+    webbrowser.open_new(link)
   else:
     print("Incorrect choice!")
     menu()
@@ -64,6 +80,7 @@ def encrypt():
     with open(file, "wb") as data:
       data.write(contents)
     sizeafter = os.path.getsize(path)
+    time.sleep(2)
     print("Encryption finished!")
     print("File size went from", sizebefore, "bytes to", sizeafter, "bytes!")
     if algorithm == "fernet":
@@ -96,15 +113,29 @@ def decrypt():
         contents = fernet.decrypt(bytes(contents, "utf-8"))
         contents = contents.decode("utf-8")
       except:
+        time.sleep(0.5)
         print("No key file was found! Make sure your key file is in the current directory ("+os.getcwd()+"), that it is named key.key and that it is not corrupted.")
         menu()
     with open(file, "wt") as data:
       data.write(contents)
     sizeafter = os.path.getsize(path)
+    time.sleep(2)
     print("Successfully decrypted file!")
     print("File size went from", sizebefore, "bytes to", sizeafter, "bytes!")
     if algorithm == "fernet":
       os.remove("key.key")
       print("key.key has been automatically deleted.")
     menu()
+try:
+  response = requests.get("https://api.github.com/repos/Uiop3385/PythonEncoderDecoder/releases/latest")
+  version = response.json()["tag_name"]
+except:
+  print("No internet connection, could not check for updates!")
+  time.sleep(3)
+  newUpdate = False
+else:
+  if version != currentVersion:
+    newUpdate = True
+  else:
+    newUpdate = False
 menu()
